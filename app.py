f04c37948a4ba374af9da3814a5068e18c7757f3
XeroSWM <wxmonteros@uce.edu.ec>
2026-02-09 08:18:57 -0500

    Subiendo código fuente y datos

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ elif menu == "🚀 Generar Horario":
     
     st.write("")
     col_opt, col_check = st.columns([1, 2])
-    modo_flex = col_check.checkbox("Modo Flexible (Permitir cruces leves)", value=False)
+    
     
     if col_opt.button("✨ Optimizar Ahora"):
         with st.spinner("Se está calculando la mejor distribución..."):
@@ -237,7 +237,7 @@ elif menu == "🚀 Generar Horario":
                 st.session_state['aulas'], 
                 carrera_activa, 
                 slots_ocupados=bloqueos_set,
-                ignorar_errores=modo_flex
+            
             )
             
             if df_nuevo is not None:
